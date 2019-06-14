85c90faabd8f627740604b57ec2b85eef2a92a47
ikidnapmyself <burakalbayrak@live.com>
2019-06-14 16:24:02 +0300

    set ledger id on init

--- src/ModelTraits/AccountingJournal.php
+++ src/ModelTraits/AccountingJournal.php
@@ -20,17 +20,19 @@ trait AccountingJournal {
     {
         return $this->morphOne(Journal::class,'morphed');
     }
-	
-	/**
-	 * Initialize a journal for a given model object
-	 *
-	 * @param string $currency_code
-	 * @return Journal
-	 * @throws \Exception
-	 */
-	public function initJournal($currency_code='USD') {
+
+    /**
+     * Initialize a journal for a given model object
+     *
+     * @param null|string $currency_code
+     * @param null|string $ledger_id
+     * @return mixed
+     * @throws \Exception
+     */
+	public function initJournal(?string $currency_code = 'USD', ?string $ledger_id = null) {
     	if (!$this->journal) {
 	        $journal = new Journal();
+	        $journal->ledger_id = $ledger_id;
 	        $journal->currency = $currency_code;
 	        $journal->balance = 0;
 	        return $this->journal()->save($journal);
