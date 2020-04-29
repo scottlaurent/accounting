38b7427821e67e0fab656415650c00c763de648e
Scott Laurent <scott@madetoorderwebsites.com>
2020-04-28 19:37:18 -0500

    exception message visibility fix

--- src/Exceptions/BaseException.php
+++ src/Exceptions/BaseException.php
@@ -8,8 +8,6 @@ use Exception;
 
 class BaseException extends Exception
 {
-    public $message;
-
     public function __construct($message = null)
     {
         parent::__construct($message ?: $this->message, 0, null);
